4c54c9f70cb2e92bee48b7889f984240aa6c7630
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-08-26 20:42:49 +0700

    Create Variable2.java

---
+++ Variable2.java
@@ -0,0 +1,23 @@
+/*
+Author : Mochammad Ihza Rizky Karim
+Program Name : Variable2.java
+*/
+
+public class Variable2{
+
+	static String nama = "Mochammad Ihza Rizky Karim";
+	static int angka = 19;
+	static char karakter = 'A';
+	static boolean statement = true;
+	static boolean statements = false;
+
+	public static void main(String[] args)
+	{
+
+		System.out.println(nama); 
+		System.out.println(angka); 
+		System.out.println(karakter); 
+		System.out.println(statement);
+		System.out.println(statements);
+	}
+}
