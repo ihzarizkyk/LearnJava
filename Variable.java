0f35a01843817e8f7ab678b3c94578587777e3a6
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-08-26 20:42:21 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,22 @@
+/*
+Author : Mochammad Ihza Rizky Karim
+Program Name : Variable.java
+*/
+
+public class Variable{
+
+	public static void main(String[] args)
+	{
+		String nama = "Mochammad Ihza Rizky Karim";
+		int angka = 19;
+		char karakter = 'A';
+		boolean statement = true;
+		boolean statements = false;
+
+		System.out.println(nama); 
+		System.out.println(angka); 
+		System.out.println(karakter); 
+		System.out.println(statement);
+		System.out.println(statements);
+	}
+}
