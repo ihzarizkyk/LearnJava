c7ce75ffcc193289fe08d5d79e664dc7437923bc
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-08-27 08:13:29 +0700

    Create data.java

---
+++ data.java
@@ -0,0 +1,34 @@
+/*
+@Author : Mochammad Ihza Rizky Karim
+Program Name : data.java
+*/
+// Tipe Data
+
+public class data{
+
+	public static void main(String[] args)
+	{
+		String nama = "andi budi";
+		int angka = 100;
+		char huruf = 'A';
+		boolean pembuktian = true;
+		String[] Nama = {"Budi","Anton","Andi"};
+		int[] Bilangan = {1,2,3,4};
+
+		System.out.println(nama);
+		System.out.println(huruf);
+		System.out.println(Bilangan[1]);
+		System.out.println(Nama[0]);
+
+	}
+}
+
+/*
+OUTPUT :
+
+andi budi - PRIMITIVE
+A - PRIMITIVE
+2 - ARRAY (NON PRIMITIVE)
+Budi - ARRAY (NON PRIMITIVE)
+
+*/
