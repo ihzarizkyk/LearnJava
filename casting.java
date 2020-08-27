74cc463a9b44c06b33e9445245986fca7971b236
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-08-27 08:31:18 +0700

    Update casting.java

--- casting.java
+++ casting.java
@@ -1,1 +1,36 @@
+/*
+@Author : Mochammad Ihza Rizky Karim
+Program Name : casting.java
+*/
 
+// Casting Data Types
+// Konversi Tipe Data
+
+public class casting{
+
+	public static void main(String[] args)
+	{
+		// Widening Casting
+		int Angka = 9;
+		double Dobel = Angka;
+
+		// Narrowing Casting
+		int Number = 10;
+		double Numbers = (double) Number;
+
+		System.out.println(Angka); 
+		System.out.println(Dobel);
+		System.out.println(Number);
+		System.out.println(Numbers);
+	}
+}
+
+/*
+OUTPUT :
+
+9
+9.0
+10
+10.0
+
+*/
